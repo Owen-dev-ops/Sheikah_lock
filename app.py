47e05ce0e4c34397385da916be638d56a73c104d
Owen <owen.t.b@icloud.com>
2025-12-22 07:07:50 -0500

    Update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # Import required libraries
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
-import mysql.connector
+import sqlite3
 
 # Configure Application
 app = Flask(__name__)
@@ -13,7 +13,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# TODO: Connect/setup MySQL database here. Do this by reading through the MySQL documentation.
+# TODO: Connect/setup SQLite database here. 
 
 @app.route("/")
-# TODO: Make @login_required in helpers.py and add here.
+# TODO: Make @login_required in helpers.py and add here. Study python decorator functions first.
